5c344c4f76e2e21f893d09d769a486141393601e
Joao Corsino <oi.corsino@gmail.com>
2023-12-21 07:08:38 -0300

    Resolvendo desafio POO DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smatrphone iPhone:");
+Smartphone iPhone = new Iphone(numero: "F2LZAJXNUZ08W", modelo: "iPhone 11 Pro Max", imei: "01010101", memoria: 64);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+Console.WriteLine("Smatrphone Nokia:");
+Smartphone nokia = new Nokia(numero: "DB1029882892920", modelo: "C21 plus", imei: "110101001", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+Console.ReadLine();
